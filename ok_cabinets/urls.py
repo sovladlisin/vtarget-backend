4e1611a3ef55c4e63c75705e8fb91841d826dc65
Vlad <vladlisin2@gmail.com>
2021-08-03 19:14:01 +0700

    ok_perms_update

--- ok_cabinets/urls.py
+++ ok_cabinets/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import getUserById
+from .views import getUserById, getUsersPermissions, changeUserPermissions
 
 urlpatterns = [
     path('ok_cabinets/api/getUserById',
          getUserById, name='getUserById'),
+    path('ok_cabinets/api/getUsersPermissions',
+         getUsersPermissions, name='getUsersPermissions'),
+    path('ok_cabinets/api/changeUserPermissions',
+         changeUserPermissions, name='changeUserPermissions'),
 ]
--- ok_cabinets/views.py
+++ ok_cabinets/views.py
@@ -43,3 +43,60 @@ def getUserById(request):
 
         return JsonResponse(response, safe=False)
     return HttpResponse('Wrong request')
+
+
+@csrf_exempt
+def getUsersPermissions(request):
+    if request.method == 'GET':
+        user_pk = request.GET.get('admin_pk', None)
+        if user_pk is None:
+            return HttpResponse(status=404)
+        user = VkUser.objects.get(pk=user_pk)
+        if user.is_admin == False:
+            return HttpResponse(status=301)
+
+        user_perms = OKUserPermissions.objects.all()
+        result = []
+        for perm in user_perms:
+            temp = {}
+            temp['user_pk'] = perm.user.pk
+            temp['permissions'] = json.loads(perm.permissions)
+            result.append(temp)
+
+        return JsonResponse(result, safe=False)
+    return HttpResponse('Wrong request')
+
+
+@csrf_exempt
+def changeUserPermissions(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+
+        admin_pk = data.get('admin_pk', None)
+        user_pk = data.get('user_pk', None)
+        permissions = data.get('permissions', None)
+
+        if admin_pk is None:
+            return HttpResponse(status=404)
+        admin = VkUser.objects.get(pk=admin_pk)
+        if admin.is_admin == False:
+            return HttpResponse(status=301)
+
+        user = VkUser.objects.get(pk=user_pk)
+        user_perm = OKUserPermissions.objects.all().filter(user__pk=user.pk)
+
+        if user_perm.count() == 0:
+            user_perm = OKUserPermissions(user=user)
+            user_perm.save()
+        else:
+            user_perm = user_perm.first()
+
+        user_perm.permissions = json.dumps(permissions)
+        user_perm.save()
+
+        response = {}
+        response['user_pk'] = user_pk
+        response['permissions'] = permissions
+
+        return JsonResponse(response, safe=False)
+    return HttpResponse('Wrong request')
