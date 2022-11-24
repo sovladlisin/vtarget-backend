7f25b795fe2dfdd239c8a15a7cb5eb89a8dfff40
vlad <vladlisin2@gmail.com>
2022-11-24 09:38:42 +0700

    fast

--- utils/certificates.py
+++ utils/certificates.py
@@ -7,11 +7,11 @@ from django.http import JsonResponse
 @csrf_exempt
 def runLegacyRequest(request):
     if request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        request_type = data.get('request_type', None)
-        data = data.get('data', None)
-        params = data.get('params', None)
-        url = data.get('url', None)
+        data_main = json.loads(request.body.decode('utf-8'))
+        request_type = data_main.get('request_type', None)
+        data = data_main.get('data', None)
+        params = data_main.get('params', None)
+        url = data_main.get('url', None)
         
         if request_type == 'POST':
             response = requests.post(url=url, data=json.dumps(data))
