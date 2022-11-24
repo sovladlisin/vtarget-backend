8fbc79e7db6aabe2ca61b9db21b0de6f2eaa593a
vlad <vladlisin2@gmail.com>
2022-11-24 10:11:08 +0700

    fast

--- utils/certificates.py
+++ utils/certificates.py
@@ -3,6 +3,7 @@ import requests
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import base64
 
 @csrf_exempt
 def runLegacyRequest(request):
@@ -18,5 +19,38 @@ def runLegacyRequest(request):
         if request_type == 'GET':
             response = requests.get(url=url, params=params).json()
 
+        return JsonResponse(response, safe=False)
+    return HttpResponse('Wrong request')
+
+def get_as_base64(url):
+
+    return base64.b64encode(requests.get(url)).content
+
+def runLegacyShutterstockGetImages(request):
+    if request.method == 'POST':
+        data_main = json.loads(request.body.decode('utf-8'))
+        data = data_main.get('data', None)
+        url = data_main.get('url', None)
+
+
+        response = requests.post(url, data).json()
+        images = response['response'].get('images', [])
+        new_images = []
+        for image in images:
+            base = get_as_base64('http://shutterstock.parsers.services.vtargete.ru:14292' + image['url'])
+            image['base'] = base
+            new_images.append(image)
+        all_images_cnt = response['response'].get('all_images_cnt', 0)
+        
+        return JsonResponse({'images': new_images, 'all_images_cnt': all_images_cnt}, safe=False)
+
+    return HttpResponse('Wrong request')
+
+def runLegacyShutterstock(request):
+    if request.method == 'POST':
+        data_main = json.loads(request.body.decode('utf-8'))
+        data = data_main.get('data', None)
+        url = data_main.get('url', None)
+        response = requests.post(url, data).json()
         return JsonResponse(response, safe=False)
     return HttpResponse('Wrong request')
--- utils/urls.py
+++ utils/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 from .views import searchVtarget, getRandWord, searchShutterstock, getTagJobStatus, startCollectTagsJob
-from .certificates import runLegacyRequest
+from .certificates import runLegacyRequest, runLegacyShutterstockGetImages, runLegacyShutterstock
 urlpatterns = [
-    path('utils/getRandWord',
-         getRandWord, name='getRandWord'),
-    path('utils/searchVtarget',
-         searchVtarget, name='searchVtarget'),
-    path('utils/searchShutterstock',
-         searchShutterstock, name='searchShutterstock'),
-    path('utils/getTagJobStatus',
-         getTagJobStatus, name='getTagJobStatus'),
-    path('utils/startCollectTagsJob',
-         startCollectTagsJob, name='startCollectTagsJob'),
+     path('utils/getRandWord',
+          getRandWord, name='getRandWord'),
+     path('utils/searchVtarget',
+          searchVtarget, name='searchVtarget'),
+     path('utils/searchShutterstock',
+          searchShutterstock, name='searchShutterstock'),
+     path('utils/getTagJobStatus',
+          getTagJobStatus, name='getTagJobStatus'),
+     path('utils/startCollectTagsJob',
+          startCollectTagsJob, name='startCollectTagsJob'),
+
      path('utils/runLegacyRequest',
-         runLegacyRequest, name='runLegacyRequest'),
+          runLegacyRequest, name='runLegacyRequest'),
+     path('utils/runLegacyShutterstockGetImages',
+          runLegacyShutterstockGetImages, name='runLegacyShutterstockGetImages'),
+     path('utils/runLegacyShutterstock',
+          runLegacyShutterstock, name='runLegacyShutterstock'),
 ]
